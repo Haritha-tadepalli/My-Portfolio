aee8c926f302cf0f8611ce47b9406386d46e44ed
Haritha-tadepalli <t.haritha032@gmail.com>
2023-04-21 08:47:27 +0530

    changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Header>
+      <div className="container header_container">
+        <h5>Hi!! I am</h5>
+        <h1>Haritha Tadepalli</h1>
+        <h5 className="text-light">FrontEnd Developer</h5>
+        <CTA />
+      </div>
+
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+    </Header>
   )
 }
 
